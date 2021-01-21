1298b7c18029202ed7941de8309222c24c8f0950
Mariano Mollo <marianomollo@pm.me>
2021-01-21 16:04:47 +0100

    List ignored locations

---
+++ 10-ignores.sh
@@ -0,0 +1,14 @@
+IgnorePath '/etc/ssl/certs/*'
+IgnorePath '/var/lib/pacman/local/*' # package metadata
+IgnorePath '/var/lib/pacman/sync/*.db' # repos
+IgnorePath '/var/lib/pacman/sync/*.db.sig' # repo sigs
+IgnorePath '/var/lib/pacman/sync/community.files'
+IgnorePath '/boot/*'
+IgnorePath '/var/tmp/*'
+IgnorePath '/var/log/journal/*'
+IgnorePath '/usr/lib/locale/*'
+IgnorePath '/usr/lib/udev/*'
+IgnorePath '/root/*'
+IgnorePath '/usr/*'
+IgnorePath '/var/*'
+IgnorePath '/etc/ca-certificates/*'
