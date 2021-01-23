da8562349ca8ee6d9eb77b7f55d626e74272e9d6
Mariano Mollo <marianomollo@pm.me>
2021-01-23 15:25:45 +0100

    Add android-tools package

--- 30-packages.sh
+++ 30-packages.sh
@@ -1,2 +1,3 @@
 AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage emacs # The extensible, customizable, self-documenting real-time display editor
+AddPackage android-tools # Android platform tools
