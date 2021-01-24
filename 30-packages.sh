4a8a2ec6f0db31d4611f74ec490453411a63070a
Mariano Mollo <marianomollo@pm.me>
2021-01-24 15:32:29 +0100

    Add packages

--- 30-packages.sh
+++ 30-packages.sh
@@ -1,3 +1,11 @@
+# Packages
+
 AddPackage firefox # Standalone web browser from mozilla.org
 AddPackage emacs # The extensible, customizable, self-documenting real-time display editor
 AddPackage android-tools # Android platform tools
+AddPackage chromium # A web browser built for speed, simplicity, and security
+AddPackage pass # Stores, retrieves, generates, and synchronizes passwords securely
+
+# Foreign packages
+
+AddPackage --foreign protonvpn-cli-ng # A Linux CLI for ProtonVPN.
