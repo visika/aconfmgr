4b4505e6fc52189f51c49ac006467b2a2f143e03
Mariano Mollo <marianomollo@protonmail.ch>
2021-04-17 16:31:51 +0200

    Aggiorna pacchetti

--- 30-packages.sh
+++ 30-packages.sh
@@ -1,11 +1,55 @@
 # Packages
 
+## Manjaro
+
+AddPackage dolphin # KDE File Manager
+AddPackage dolphin-plugins # Extra Dolphin plugins
+AddPackage gwenview # A fast and easy to use image viewer
+AddPackage kamera # KDE integration for gphoto2 cameras
+AddPackage kate # Advanced Text Editor
+AddPackage kcalc # Scientific Calculator
+AddPackage kdeconnect # Adds communication between KDE and your smartphone
+AddPackage konsole # KDE's terminal emulator
+AddPackage partitionmanager # A KDE utility that allows you to manage disks, partitions, and file systems
+AddPackage spectacle # KDE screenshot capture utility
 AddPackage firefox # Standalone web browser from mozilla.org
-AddPackage emacs # The extensible, customizable, self-documenting real-time display editor
+AddPackage firefox-i18n-en-us # English (US) language pack for Firefox
+AddPackage firefox-i18n-it # Italian language pack for Firefox
+AddPackage hunspell-en_us # US English hunspell dictionaries
+AddPackage hunspell-it # Italian dictionary for Hunspell
+AddPackage man-pages-it # Italian man pages
+AddPackage qt5-translations # A cross-platform application and UI framework (Translations)
+
+## Personal
+
+AddPackage brave # Web browser that blocks ads and trackers by default (latest binary release).
 AddPackage android-tools # Android platform tools
-AddPackage chromium # A web browser built for speed, simplicity, and security
 AddPackage pass # Stores, retrieves, generates, and synchronizes passwords securely
+AddPackage kamoso # A webcam recorder from KDE community
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage texlive-science # TeX Live - Typesetting for mathematics, natural and computer sciences
+# AddPackage chromium # A web browser built for speed, simplicity, and security
+# AddPackage code # The Open Source build of Visual Studio Code (vscode) editor
+# AddPackage discord # All-in-one voice and text chat for gamers that's free and secure.
+# AddPackage handbrake # Multithreaded video transcoder
+# AddPackage leiningen # Automate Clojure projects
+# AddPackage pandoc # Conversion between markup formats
+# AddPackage signal-desktop # Signal Private Messenger for Linux
+# AddPackage telegram-desktop # Official Telegram Desktop client
+# AddPackage youtube-dl # A command-line program to download videos from YouTube.com and a few more sites
+# AddPackage peek # Simple screen recorder with an easy to use interface
+
+## Doom emacs
+AddPackage emacs # The extensible, customizable, self-documenting real-time display editor
+AddPackage fd # Simple, fast and user-friendly alternative to find
+AddPackage neovim # Fork of Vim aiming to improve user experience, plugins, and GUIs
+AddPackage ripgrep # A search tool that combines the usability of ag with the raw speed of grep
+
 
 # Foreign packages
 
-AddPackage --foreign protonvpn-cli-ng # A Linux CLI for ProtonVPN.
+AddPackage --foreign aconfmgr-git # A configuration manager for Arch Linux
+# AddPackage --foreign protonvpn-cli-ng # A Linux CLI for ProtonVPN.
+# AddPackage --foreign spotify-adblock-git # Adblocker for Spotify
+# AddPackage --foreign zettlr-bin # A markdown editor for writing academic texts and taking notes
+# AddPackage --foreign zoom # Video Conferencing and Web Conferencing Service
