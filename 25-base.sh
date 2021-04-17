ae457d2c0059e144333c0f2805a4b781fc58f862
Mariano Mollo <marianomollo@protonmail.ch>
2021-04-17 16:32:15 +0200

    aggiungi pacchetti di base

---
+++ 25-base.sh
@@ -0,0 +1,241 @@
+AddPackage acpi # Client for battery, power, and thermal readings
+AddPackage acpid # A daemon for delivering ACPI power management events with netlink support
+AddPackage bash # The GNU Bourne Again shell
+AddPackage grep # A string search utility
+AddPackage less # A terminal based program for viewing text files
+AddPackage sudo # Give certain users the ability to run some commands as root
+AddPackage plasma-systemmonitor # An interface for monitoring system sensors, process information and other system resources
+AddPackage alsa-firmware # Firmware binaries for loader programs in alsa-tools and hotplug firmware loader
+AddPackage alsa-utils # Advanced Linux Sound Architecture - Utilities
+AddPackage android-udev # Udev rules to connect Android devices to your linux box
+AddPackage ark # Archiving Tool
+AddPackage avahi # Service Discovery for Linux using mDNS/DNS-SD -- compatible with Bonjour
+AddPackage b43-fwcutter # firmware extractor for the b43 kernel module
+AddPackage bluedevil # Integrate the Bluetooth technology within KDE workspace and applications
+AddPackage btrfs-progs # Btrfs filesystem utilities
+AddPackage bzip2 # A high-quality data compression program
+AddPackage coreutils # The basic file, shell and text manipulation utilities of the GNU operating system
+AddPackage cpupower # Linux kernel tool to examine and tune power saving related features of your processor
+AddPackage crda # Central Regulatory Domain Agent for wireless networks
+AddPackage cronie # Daemon that runs specified programs at scheduled times and related tools
+AddPackage cryptsetup # Userspace setup tool for transparent encryption of block devices using dm-crypt
+AddPackage device-mapper # Device mapper userspace library and tools
+AddPackage dhclient # A standalone DHCP client from the dhcp package
+AddPackage dhcpcd # RFC2131 compliant DHCP client daemon
+AddPackage diffutils # Utility programs used for creating patch files
+AddPackage dmidecode # Desktop Management Interface table related utilities
+AddPackage dmraid # Device mapper RAID interface
+AddPackage dnsmasq # Lightweight, easy to configure DNS forwarder and DHCP server
+AddPackage dosfstools # DOS filesystem utilities
+AddPackage e2fsprogs # Ext2/3/4 filesystem utilities
+AddPackage ecryptfs-utils # Enterprise-class stacked cryptographic filesystem for Linux
+AddPackage efibootmgr # Linux user-space application to modify the EFI Boot Manager
+AddPackage exfat-utils # Utilities for exFAT file system
+AddPackage f2fs-tools # Tools for Flash-Friendly File System (F2FS)
+AddPackage ffmpeg # Complete solution to record, convert and stream audio and video
+AddPackage ffmpegthumbs # FFmpeg-based thumbnail creator for video files
+AddPackage file # File type identification utility
+AddPackage filesystem # Base Manjaro Linux files
+AddPackage findutils # GNU utilities to locate files
+AddPackage gawk # GNU version of awk
+AddPackage gcc-libs # Runtime libraries shipped by GCC
+AddPackage gettext # GNU internationalization library
+AddPackage glibc # GNU C Library
+AddPackage gnome-icon-theme # GNOME icon theme
+AddPackage gnome-themes-extra # Extra Themes for GNOME Applications
+AddPackage grub # GNU GRand Unified Bootloader (2)
+AddPackage grub-theme-manjaro # Manjaro Linux grub theme
+AddPackage gst-libav # Multimedia graph framework - libav plugin
+AddPackage gst-plugins-bad # Multimedia graph framework - bad plugins
+AddPackage gst-plugins-base # Multimedia graph framework - base plugins
+AddPackage gst-plugins-good # Multimedia graph framework - good plugins
+AddPackage gst-plugins-ugly # Multimedia graph framework - ugly plugins
+AddPackage gvfs # Virtual filesystem implementation for GIO
+AddPackage gvfs-afc # Virtual filesystem implementation for GIO (AFC backend; Apple mobile devices)
+AddPackage gvfs-gphoto2 # Virtual filesystem implementation for GIO (gphoto2 backend; PTP camera, MTP media player)
+AddPackage gvfs-mtp # Virtual filesystem implementation for GIO (MTP backend; Android, media player)
+AddPackage gvfs-nfs # Virtual filesystem implementation for GIO (NFS backend)
+AddPackage gvfs-smb # Virtual filesystem implementation for GIO (SMB/CIFS backend; Windows client)
+AddPackage gzip # GNU compression utility
+AddPackage htop # Interactive process viewer
+AddPackage illyria-wallpaper # Official Wallpapers for the Manjarolinux 18.x release
+AddPackage inetutils # A collection of common network programs
+AddPackage intel-ucode # Microcode update files for Intel CPUs
+AddPackage inxi # Full featured CLI system information tool
+AddPackage iproute2 # IP Routing Utilities
+AddPackage iputils # Network monitoring tools, including ping
+AddPackage jfsutils # JFS filesystem utilities
+AddPackage kaccounts-providers # Small system to administer web accounts for the sites and services across the KDE desktop, including
+AddPackage kwin # An easy to use, but flexible, composited Window Manager
+AddPackage pacman # A library-based package manager with dependency support
+AddPackage pamac-cli # A Package Manager based on libalpm with AUR and Appstream support
+AddPackage pamac-gtk # A Package Manager based on libalpm with AUR and Appstream support
+AddPackage pamac-tray-icon-plasma # Pamac tray icon for plasma users
+AddPackage plasma-desktop # KDE Plasma Desktop
+AddPackage plasma-nm # Plasma applet written in QML for managing network connections
+AddPackage plasma-pa # Plasma applet for audio volume management using PulseAudio
+AddPackage plasma-simplemenu # Simplified menu for the plasma desktop
+AddPackage plasma-workspace # KDE Plasma Workspace
+AddPackage adobe-source-han-sans-cn-fonts # Adobe Source Han Sans Subset OTF - Simplified Chinese OpenType/CFF fonts
+AddPackage adobe-source-han-sans-jp-fonts # Adobe Source Han Sans Subset OTF - Japanese OpenType/CFF fonts
+AddPackage adobe-source-han-sans-kr-fonts # Adobe Source Han Sans Subset OTF - Korean OpenType/CFF fonts
+AddPackage adobe-source-sans-pro-fonts # Sans-serif font family for user interface environments
+AddPackage kde-gtk-config # GTK2 and GTK3 Configurator for KDE
+AddPackage kdegraphics-thumbnailers # Thumbnailers for various graphics file formats
+AddPackage kdenetwork-filesharing # Properties dialog plugin to share a directory with the local network
+AddPackage kdeplasma-addons # All kind of addons to improve your Plasma experience
+AddPackage kfind # Find Files/Folders
+AddPackage kgamma5 # Adjust your monitor's gamma settings
+AddPackage khelpcenter # Application to show KDE Applications' documentation
+AddPackage khotkeys # KHotKeys
+AddPackage kimageformats # Image format plugins for Qt5
+AddPackage kinfocenter # A utility that provides information about a computer system
+AddPackage kio-extras # Additional components to increase the functionality of KIO
+AddPackage kmenuedit # KDE menu editor
+AddPackage konversation # A user-friendly and fully-featured IRC client
+AddPackage kscreen # KDE's screen management software
+AddPackage kscreenlocker # Library and components for secure lock screen architecture
+AddPackage ksshaskpass # ssh-add helper that uses kwallet and kpassworddialog
+AddPackage ksysguard # Track and control the processes running in your system
+AddPackage ksystemlog # System log viewer tool
+AddPackage kwallet-pam # KWallet PAM integration
+AddPackage kwalletmanager # Wallet management tool
+AddPackage kwayland-integration # Provides integration plugins for various KDE frameworks for the wayland windowing system
+AddPackage kwrited # KDE daemon listening for wall and write messages
+AddPackage lib32-libcanberra # A small and lightweight implementation of the XDG Sound Theme Specification (32-bit)
+AddPackage lib32-libcanberra-pulse # PulseAudio plugin for libcanberra (32-bit)
+AddPackage lib32-libva-intel-driver # VA-API implementation for Intel G45 and HD Graphics family (32-bit)
+AddPackage lib32-libva-mesa-driver # VA-API implementation for gallium (32-bit)
+AddPackage lib32-libva-vdpau-driver # VDPAU backend for VA API (32-bit)
+AddPackage lib32-mesa-demos # Mesa demos and tools (32-bit)
+AddPackage lib32-mesa-vdpau # Mesa VDPAU drivers (32-bit)
+AddPackage lib32-vulkan-intel # Intel's Vulkan mesa driver (32-bit)
+AddPackage lib32-vulkan-radeon # Radeon's Vulkan mesa driver (32-bit)
+AddPackage libcanberra # A small and lightweight implementation of the XDG Sound Theme Specification
+AddPackage libcanberra-gstreamer # GStreamer plugin for libcanberra
+AddPackage libcanberra-pulse # PulseAudio plugin for libcanberra
+AddPackage libdvdcss # Portable abstraction library for DVD decryption
+AddPackage libva-intel-driver # VA-API implementation for Intel G45 and HD Graphics family
+AddPackage libva-mesa-driver # VA-API implementation for gallium
+AddPackage libva-vdpau-driver # VDPAU backend for VA API
+AddPackage licenses # Standard licenses distribution package
+AddPackage linux510 # The Linux510 kernel and modules
+AddPackage logrotate # Rotates system logs automatically
+AddPackage lvm2 # Logical Volume Manager 2 utilities
+AddPackage man-db # A utility for reading man pages
+AddPackage man-pages # Linux man pages
+AddPackage manjaro-alsa # manjaro ALSA support (Meta-PKG)
+AddPackage manjaro-application-utility # Manjaro Application Utility
+AddPackage manjaro-browser-settings # Manjaro Linux settings browser defaults
+AddPackage manjaro-documentation-en # Manjaro Beginner User Guide in English
+AddPackage manjaro-firmware # Extra firmwares for Manjaro Linux
+AddPackage manjaro-hello # A tool providing access to documentation and support for new Manjaro users.
+AddPackage manjaro-hotfixes # Manjaro Linux Hotfixes
+AddPackage manjaro-kde-settings # Manjaro Linux KDE settings
+AddPackage manjaro-pulse # manjaro Pulseaudio support (Meta-PKG)
+AddPackage manjaro-release # Manjaro's release definition
+AddPackage manjaro-settings-manager-kcm # Manjaro Linux System Settings Tool (KCM for Plasma 5)
+AddPackage manjaro-settings-manager-knotifier # Manjaro Linux System Settings Tool (Notifier for Plasma 5)
+AddPackage manjaro-system # Manjaro Linux System - Update script
+AddPackage manjaro-zsh-config # Zsh configuration for manjaro
+AddPackage mdadm # A tool for managing/monitoring Linux md device arrays, also known as Software RAID
+AddPackage memtest86+ # An advanced memory diagnostic tool
+AddPackage mesa-demos # Mesa demos and tools incl. glxinfo + glxgears
+AddPackage mesa-vdpau # Mesa VDPAU drivers
+AddPackage mhwd # Manjaro Linux Hardware Detection library and application
+AddPackage mhwd-db # Manjaro Linux Hardware Detection Database
+AddPackage milou # A dedicated search application built on top of Baloo
+AddPackage mkinitcpio-openswap # mkinitcpio hook to open swap at boot time
+AddPackage mobile-broadband-provider-info # Network Management daemon
+AddPackage modemmanager # Mobile broadband modem management service
+AddPackage mtpfs # A FUSE filesystem that supports reading and writing from any MTP device
+AddPackage nano # Pico editor clone with enhancements
+AddPackage networkmanager # Network connection manager and user applications
+AddPackage networkmanager-openconnect # NetworkManager VPN plugin for OpenConnect
+AddPackage networkmanager-openvpn # NetworkManager VPN plugin for OpenVPN
+AddPackage networkmanager-pptp # NetworkManager VPN plugin for PPTP
+AddPackage networkmanager-vpnc # NetworkManager VPN plugin for VPNC
+AddPackage nextcloud-client # Nextcloud desktop client
+AddPackage nfs-utils # Support programs for Network File Systems
+AddPackage noto-fonts # Google Noto TTF fonts
+AddPackage nss-mdns # glibc plugin providing host name resolution via mDNS
+AddPackage ntfs-3g # NTFS filesystem driver and utilities
+AddPackage ntp # Network Time Protocol reference implementation
+AddPackage numlockx # Turns on the numlock key in X11.
+AddPackage okular # Document Viewer
+AddPackage openresolv # resolv.conf management framework (resolvconf)
+AddPackage openssh # Premier connectivity tool for remote login with the SSH protocol
+AddPackage os-prober # Utility to detect other OSes on a set of drives
+AddPackage oxygen # KDE Oxygen style
+AddPackage oxygen-icons # The Oxygen Icon Theme
+AddPackage p7zip # Command-line file archiver with high compression ratio
+AddPackage pciutils # PCI bus configuration space access library and tools
+AddPackage perl # A highly capable, feature-rich programming language
+AddPackage perl-file-mimeinfo # Determine file type, includes mimeopen and mimetype
+AddPackage phonon-qt5-gstreamer # Phonon GStreamer backend for Qt5
+AddPackage poppler-data # Encoding data for the poppler PDF rendering library
+AddPackage powerdevil # Manages the power consumption settings of a Plasma Shell
+AddPackage powertop # A tool to diagnose issues with power consumption and power management
+AddPackage procps-ng # Utilities for monitoring your system and its processes
+AddPackage psmisc # Miscellaneous procfs tools
+AddPackage pulseaudio-bluetooth # Bluetooth support for PulseAudio
+AddPackage pulseaudio-ctl # Control pulseaudio volume from the shell or mapped to keyboard shortcuts.
+AddPackage pulseaudio-zeroconf # Zeroconf support for PulseAudio
+AddPackage qt5-imageformats # Plugins for additional image formats
+AddPackage qt5-virtualkeyboard # Virtual keyboard framework
+AddPackage reiserfsprogs # Reiserfs utilities
+AddPackage rsync # A fast and versatile file copying tool for remote and local files
+AddPackage ruby # An object-oriented language for quick and easy programming
+AddPackage s-nail # Environment for sending and receiving mail
+AddPackage samba # SMB Fileserver and AD Domain server
+AddPackage screenfetch # CLI Bash script to show system/theme info in screenshots
+AddPackage sddm # QML based X11 and Wayland display manager
+AddPackage sddm-breath2-theme # breath2 theme for SDDM
+AddPackage sddm-kcm # KDE Config Module for SDDM
+AddPackage sed # GNU stream editor
+AddPackage shadow # Password and account management tool suite with support for shadow files and PAM
+AddPackage spectre-meltdown-checker # Spectre, Meltdown, Foreshadow, Fallout, RIDL, ZombieLoad vulnerability/mitigation checker
+AddPackage squashfs-tools # Tools for squashfs, a highly compressed read-only filesystem for Linux.
+AddPackage sshfs # FUSE client based on the SSH File Transfer Protocol
+AddPackage sysfsutils # System Utilities Based on Sysfs
+AddPackage systemd # system and service manager
+AddPackage systemd-fsck-silent # File system checks for SystemD (silent)
+AddPackage systemd-kcm # Systemd control module for KDE
+AddPackage systemd-sysvcompat # sysvinit compat for systemd
+AddPackage systemsettings # KDE system manager for hardware, software, and workspaces
+AddPackage tar # Utility used to store, backup, and transport files
+AddPackage terminus-font # Monospace bitmap font (for X11 and console)
+AddPackage texinfo # GNU documentation system for on-line information and printed output
+AddPackage tlp # Linux Advanced Power Management
+AddPackage ttf-dejavu # Font family based on the Bitstream Vera Fonts with a wider range of characters
+AddPackage udiskie # Removable disk automounter using udisks
+AddPackage udisks2 # Disk Management Service, version 2
+AddPackage unarchiver # unar and lsar
+AddPackage usb_modeswitch # Activating switchable USB devices on Linux.
+AddPackage usbutils # A collection of USB tools to query connected USB devices
+AddPackage util-linux # Miscellaneous system utilities for Linux
+AddPackage vi # The original ex/vi text editor
+AddPackage vulkan-intel # Intel's Vulkan mesa driver
+AddPackage vulkan-radeon # Radeon's Vulkan mesa driver
+AddPackage wget # Network utility to retrieve files from the Web
+AddPackage which # A utility to show the full path of commands
+AddPackage wpa_supplicant # A utility providing key negotiation for WPA wireless networks
+AddPackage xdg-user-dirs # Manage user directories like ~/Desktop and ~/Music
+AddPackage xdg-utils # Command line tools that assist applications with a variety of desktop integration tasks
+AddPackage xf86-input-elographics # X.org Elographics TouchScreen input driver
+AddPackage xf86-input-evdev # X.org evdev input driver
+AddPackage xf86-input-libinput # Generic input driver for the X.Org server based on libinput
+AddPackage xf86-input-void # X.org void input driver
+AddPackage xf86-video-amdgpu # X.org amdgpu video driver
+AddPackage xf86-video-ati # X.org ati video driver
+AddPackage xf86-video-intel # X.org Intel i810/i830/i915/945G/G965+ video drivers
+AddPackage xf86-video-nouveau # Open Source 3D acceleration driver for nVidia cards
+AddPackage xfsprogs # XFS filesystem utilities
+AddPackage xorg-mkfontscale # Create an index of scalable font files for X
+AddPackage xorg-server # Xorg X server
+AddPackage xorg-twm # Tab Window Manager for the X Window System
+AddPackage xorg-xinit # X.Org initialisation program
+AddPackage xorg-xkill # Kill a client by its X resource
+AddPackage xz # Library and command line tools for XZ and LZMA compressed files
+AddPackage yakuake # A drop-down terminal emulator based on KDE konsole technology
+AddPackage zensu # A simple gksu replacment using zenity, yad, kdialog or spacefm
